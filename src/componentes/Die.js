74e09ae1e145123aa32815ca8719a7f6eedd5f51
elias <hernandezelas@gmail.com>
2022-11-08 23:39:16 -0600

    Creando el  componente Die.js

---
+++ src/componentes/Die.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import '../hojasEstilos/Die.css'
+
+export default function Die(props) {
+  return (
+    <div className='die-face'>
+        <h2 className='die-num'>{props.value}</h2>
+    </div>
+  )
+}
